6091ae97beefde1461beb0dd2e1dbe17cdcd10ef
Edvard <ecschoye@stud.ntnu.no>
2023-10-05 17:39:51 +0200

    06-01

---
+++ exercises/06/One/README.md
@@ -0,0 +1,13 @@
+Combination so far: gYKV
+
+Combination so far: nXvS
+
+Combination so far: uWgP
+
+Combination so far: BURN
+
+Combination so far: ITCK
+
+Combination so far: PSnH
+
+Password found: QwE
--- exercises/06/One/main.cpp
+++ exercises/06/One/main.cpp
@@ -13,24 +13,8 @@ std::string hex(const std::string &input) {
     return hex_stream.str();
 }
 
-std::string sha512(const std::string &input) noexcept {
-    auto evp_md = EVP_sha512();
-
-    std::string hash(SHA512_DIGEST_LENGTH, '\0');
-
-    auto ctx = EVP_MD_CTX_create();
-    EVP_MD_CTX_init(ctx);
-    EVP_DigestInit_ex(ctx, evp_md, nullptr);
-    EVP_DigestUpdate(ctx, input.data(), input.size());
-    EVP_DigestFinal_ex(ctx, reinterpret_cast<unsigned char *>(&hash[0]), nullptr);
-    EVP_MD_CTX_destroy(ctx);
-
-    return hash;
-}
-
 std::string pbkdf2(const std::string &password, const std::string &salt, int iterations) noexcept {
     const auto hash_length = SHA_DIGEST_LENGTH;
-    auto evp_md = EVP_sha1();
 
     std::string hash(SHA_DIGEST_LENGTH, '\0');
 
@@ -38,14 +22,23 @@ std::string pbkdf2(const std::string &password, const std::string &salt, int ite
             password.c_str(), password.size(),
             reinterpret_cast<const unsigned char *>(salt.c_str()), salt.size(),
             iterations,
-            EVP_sha1(), // use SHA1
+            EVP_sha1(),
             hash_length, reinterpret_cast<unsigned char *>(&hash[0])
     );
 
     return hash;
 }
 
+static int counter = 0;
+
 void check_password_combination(const std::string& alphabets, std::string& current_combination, const std::string& salt, const std::string& known_hash, int max_depth) {
+    counter++;
+
+    if (counter % 1000000 == 0) {
+        std::cout << "Combination so far: " << current_combination << std::endl;
+        counter = 0;
+    }
+
     if (current_combination.size() > max_depth) {
         return;
     }
@@ -70,7 +63,7 @@ int main() {
     std::string key = "ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6";
     std::string salt = "Saltet til Ola";
     std::string current_combination;
-    int max_depth = 7;
+    int max_depth = 3;
 
     check_password_combination(alphabets, current_combination, salt, key, max_depth);
 
