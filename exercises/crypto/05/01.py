c9dad652781a6a0d1382ba5ca362bc1696201460
ecschoye <ecschoye@stud.ntnu.no>
2023-11-21 20:57:26 +0100

    crypto/05/01

--- exercises/crypto/05/01.py
+++ exercises/crypto/05/01.py
@@ -1,3 +1,11 @@
+"""
+Alice uses p=47, q=83, e=3 as the public key for her RSA-based signature.
+
+1. What is Alice's private key?
+2. Verify if 964 is a valid signature for the message m=100.
+3. Alice wants to encrypt and sign the message 100 to Bob. What are the exact steps?
+   Assume Bob has the RSA with public key (n_b, e_b) = (3127, 33).
+"""
 
 def find_key(_p, _q, _e):
     phi = (_p - 1) * (_q - 1)
@@ -8,10 +16,16 @@ def find_key(_p, _q, _e):
 
 
 def check_if_signature_is_valid(_m, _s, _e, _n):
-    return (_s ** _e) % _n == m
+    return pow(_s, _e, _n) == _m
 
-def encrypt(_m, _e, _n):
-    return (_m ** _e) % _n
+
+def sign_message(_m, _d, _n):
+    return pow(_m, _d, _n)
+
+
+def encrypt_message(_m, _e, _n):
+    return pow(_m, _e, _n)
+
 
 if __name__ == '__main__':
     p = 47
@@ -24,10 +38,16 @@ if __name__ == '__main__':
 
     m = 100
     s = 964
-    print(f'is signature valid? {check_if_signature_is_valid(m, s, e, n)}')
+    valid_signature = check_if_signature_is_valid(m, s, e, n)
+    print(f'is signature valid? {valid_signature}')
     print()
 
     n_b = 3127
     e_b = 33
-    # Steps to encrypt: message ** e_b % n_b
-    print(f'encrypted message: {encrypt(m, e_b, n_b)}')
+    # Steps to sign: message ** d % n
+    signed_message = sign_message(m, d, n)
+    print(f'signed message: {signed_message}')
+
+    # Steps to encrypt: encrypted_message ** e_b % n_b
+    encrypted_message = encrypt_message(signed_message, e_b, n_b)
+    print(f'decrypted message: {encrypted_message}')
